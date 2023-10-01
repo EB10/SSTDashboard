e3ca66d9a82c970f6d99f1cb6e29e24f25b85726
EB10 <43236725+EB10@users.noreply.github.com>
2023-10-01 16:43:01 +0200

    Update main_dashboard.py

--- main_dashboard.py
+++ main_dashboard.py
@@ -14,7 +14,7 @@ def load_excel(file_path):
     return pd.read_excel(file_path)
 
 st.set_page_config(layout="wide")
-with open( "app\style.css" ) as css:
+with open( "style.css" ) as css:
     st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
 
 image = Image.open(r"SSTLogo.png")
