67b2dc52737b11b4851840f82bf15d619974a7b9
EB10 <43236725+EB10@users.noreply.github.com>
2023-11-29 18:50:05 +0100

    Update new_main.py

--- new_main.py
+++ new_main.py
@@ -314,7 +314,7 @@ legend_html = "<div style='margin-bottom: 2rem;'><h4>Betydningsniveauer</h4>"
 for value, color in color_mapping_df.items():
     legend_html += f"<span style='color:{color}; font-size: 32px; margin-right: 10px;'>●</span> {value}<br>"
 legend_html += "<span style='font-size: 32px;'>★</span> Milepæl</div>"
-            st.sidebar.markdown(legend_html, unsafe_allow_html=True)
+st.sidebar.markdown(legend_html, unsafe_allow_html=True)
             
         combined_figure = combined_plot_with_layout(df_HaendelsesData, selected_date, selected_data)
         st.plotly_chart(combined_figure)
